7fc4efa6fdaa53dc1bd0bd9bdabac8c1ce6a571e
gavintobin <gavintobin714@gmailcom>
2023-05-09 17:36:43 +0000

    try1

---
+++ math/linear_algebra/3-flip_me_over.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' taask  3 with hopefully eniough documentation necessary to pass checker'''
+
+def matrix_transpose(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+    transposed = [[0 for j in range(rows)] for i in range(cols)]
+    for i in range(cols):
+        for j in range(rows):
+            transposed[i][j] = matrix[j][i]
+    return transposed
