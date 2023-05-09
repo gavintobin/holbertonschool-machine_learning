97cee0b311d9b70030d4179e36f15a608102f271
gavintobin <gavintobin714@gmailcom>
2023-05-09 17:45:39 +0000

    tabbing

--- math/linear_algebra/4-line_up.py
+++ math/linear_algebra/4-line_up.py
@@ -7,5 +7,7 @@ def add_arrays(arr1, arr2):
         sum = []
         for i in range len(arr1):
             sum.append(arr1[i] + arr2[i])
-    return sum
+        return sum
+    else:
+        return None
 
