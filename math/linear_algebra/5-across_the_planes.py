c54d409e8e41b3bede8396892773fdf4250d356c
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:13:32 -0500

    fixed code

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -5,5 +5,6 @@
 def add_matrixes2d(mat1, mat2):
     for i in range(len(mat1)):
         for j in range(len(mat1[0])):
+            sum = []
             sum[i][j] = mat1[i][j] + mat2[i][j]
     return sum
