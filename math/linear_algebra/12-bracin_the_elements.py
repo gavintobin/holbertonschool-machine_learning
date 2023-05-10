a49bbf16c9ac8861d9619a6b45f5e2fb2a47e549
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 14:50:08 -0500

    same

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -4,4 +4,4 @@
 
 def np_elementwise(mat1, mat2):
     '''my ump my nump my nump'''
-    return(mat1 + mat2, mat1 * mat2, mat1 / mat 2, mat1 - mat2)
+    return(mat1 + mat2, mat1 * mat2, mat1 / mat2, mat1 - mat2)
