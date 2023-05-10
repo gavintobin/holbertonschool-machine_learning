2575478ae270a8dea9c2f191f0f14ced66b64e7b
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:35:08 -0500

    agaging

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -4,15 +4,15 @@
 
 def cat_matrices2D(mat1, mat2, axis=0):
     if axis == 0:
-            if len(mat1[0]) != len(mat2[0]):
-                return None
-            else:
-                arr3 = mat1 + mat2
-        elif axis == 1:
-            if len(mat1) != len(mat2):
-                return None
-            else:
-                arr3 = [x + x2 for x, x2 in zip(mat1, mat2)]
+        if len(mat1[0]) != len(mat2[0]):
+            return None
+        else:
+            arr3 = mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            arr3 = [x + x2 for x, x2 in zip(mat1, mat2)]
         return arr3
         
 
