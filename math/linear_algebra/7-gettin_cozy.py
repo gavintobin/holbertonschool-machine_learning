96c01db67f7219eff25bb1a77e6f3299c3021933
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:22:59 -0500

    c

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -5,6 +5,7 @@
 def cat_matrices2D(mat1, mat2, axis=0):
     '''doggy boy'''
     if axis == 0:
-        return mat1 + mat2
+        tree = mat1 + mat2
+        return tree
     else:
         return None
