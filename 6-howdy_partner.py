2e3d7e2ba13eeeb8c925a768d7970606e57672c0
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:53:28 -0500

    np

--- 6-howdy_partner.py
+++ 6-howdy_partner.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """ documention """
+import numpy as np
 
 
 def cat_arrays(arr1, arr2):
     """kitty cat"""
-    cat =[]
-    cat = arr1[i] + arr2[i]
-return cat
+    cat = np.concatenate((arr1 + arr2))
+    return cat
