5cfccffb7a9974694d2e26d874e08ecef0758cdd
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 14:48:36 -0500

    opps

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+''' my lovely lady numps'''
+
+
+def np_elementwise(mat1, mat2):
+    '''my ump my nump my nump'''
+    return (mat1 + mat2, mat1 * mat2, mat1 / mat 2, mat1 - mat2)
