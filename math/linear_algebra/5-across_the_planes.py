1cffbfcfcabeb3f777bde8bd0e9ceedd43be643c
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:33:30 -0500

    tabbing

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -8,8 +8,9 @@
 
 
 def add_matrices2D(mat1, mat2):
+    """func to add matrix"""
     if len(mat1) == len(mat2):
         result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-    return result
-    else Exception:
+        return result
+    else:
         return None
