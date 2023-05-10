2e8cd561dbfe81304bb0ec33853dc1894c465eb0
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:13:31 -0500

    task7

---
+++ math/linear_algebra/vi 7-gettin_cozy.py
@@ -0,0 +1,10 @@
+#1/usr/bin/env python3
+''' doc for cozy boy'''
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    '''doggy boy'''
+    if axis == 0:
+        return mat1 + mat2
+    else:
+        return None
