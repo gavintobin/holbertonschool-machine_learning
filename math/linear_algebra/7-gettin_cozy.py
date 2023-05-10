55f4f822f0aaa51584426f4d89e31f6df906a17b
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:31:02 -0500

    final

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -6,13 +6,14 @@ def cat_matrices2D(mat1, mat2, axis=0):
     '''doggy boy'''
     def cat_matrices2D(mat1, mat2, axis=0):
         if axis == 0:
-            if len(mat1[0]) != len(mat2[0]):
-                return None
-            return mat1 + mat2
-        elif axis == 1:
-            if len(mat1) != len(mat2):
-                return None
-            return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
-        else:
+        if len(mat1[0]) != len(mat2[0]):
             return None
+        else:
+            arr3 = mat1 + mat2
+    elif axis == 1:
+        if len(mat1) != len(mat2):
+            return None
+        else:
+            arr3 = [x + x2 for x, x2 in zip(mat1, mat2)]
+    return arr3
 
