99adb92e8b1853540e10ed35ffc8c0a1e977bdcf
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 14:02:30 -0500

    num of lines

--- math/linear_algebra/9-let_the_butcher_slice_it.py
+++ math/linear_algebra/9-let_the_butcher_slice_it.py
@@ -8,18 +8,3 @@ mat3 =  matrix[-3:, -3:]
 print("The middle two rows of the matrix are:\n{}".format(mat1))
 print("The middle two columns of the matrix are:\n{}".format(mat2))
 print("The bottom-right, square, 3x3 matrix is:\n{}".format(mat3))
-alexa@ubuntu-xenial:linear_algebra$ ./9-let_the_butcher_slice_it.py 
-The middle two rows of the matrix are:
-[[ 7  8  9 10 11 12]
- [13 14 15 16 17 18]]
-The middle two columns of the matrix are:
-[[ 3  4]
- [ 9 10]
- [15 16]
- [21 22]]
-The bottom-right, square, 3x3 matrix is:
-[[10 11 12]
- [16 17 18]
- [22 23 24]]
-
-
