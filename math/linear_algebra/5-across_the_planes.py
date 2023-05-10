11017772d1defff66eeeaac4fcb47ab978becff4
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:11:21 -0500

    try1

---
+++ math/linear_algebra/5-across_the_planes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ env bash
+''' function to add two matrixes'''
+
+
+def add_matrixes2d(mat1, mat2):
+    for i in range(len(mat1)):
+        for j in range(len(mat1[0])):
+            sum[i][j] = mat1[i][j] + mat2[i][j]
+    return sum
