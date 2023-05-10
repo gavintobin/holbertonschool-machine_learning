591527366d0b2c5567d41b24f729cce5def47a3f
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:07:11 -0500

    hopefully bow

--- 6-howdy_partner.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-""" documention """
-
-
-def cat_arrays(arr1, arr2):
-    """kitty """
-
-    dog = arr1 + arr2
-    return dog
--- math/linear_algebra/6-howdy_partner.py
+++ math/linear_algebra/6-howdy_partner.py
@@ -3,7 +3,7 @@
 
 
 def cat_arrays(arr1, arr2):
-    """kitty cat""
-    cat =[]
-    cat = arr1[i] + arr2[i]
-    return cat
+    """kitty """
+
+    dog = arr1 + arr2
+    return dog
