7f20c86b1d1f83854fef2549d1b65ceb8d107cf1
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:44:51 -0500

    parenth

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@
 
 def add_matrices2D(mat1, mat2):
     """func to add matrix"""
-    if len(mat1) == len(mat2) and len(mat1[0] == len(mat2[0])):
+    if len(mat1) == len(mat2) and len(mat1[0]) == len(mat2[0]):
         result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
         return result
     else:
