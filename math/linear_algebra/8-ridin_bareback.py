80f680ffecb74ae58b7f9e99169f3b3ed3c8834e
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 13:48:11 -0500

    trying z ip

--- math/linear_algebra/8-ridin_bareback.py
+++ math/linear_algebra/8-ridin_bareback.py
@@ -4,12 +4,6 @@
 
 def mat_mul(mat1, mat2):
     '''mulmatmulnatmulmat'''
-    for i in range(len(mat1)):
-        '''row it'''
-        for j in range(len(mat2[0])):
-            '''col it'''
-            for k in range(len(mat2)):
-                '''it row 2nd mat'''
-                resu[i][j] = mat1[i][k] * mat2[k][j]
-                return resu
+    return [[sum(a * b for a, b in zip(row, col)) for col in zip(*mat2)] for row in mat1]
+
             
