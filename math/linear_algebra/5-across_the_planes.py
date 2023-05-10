b2daa1dc17c8b86bad86bba8fc07feba71a997be
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-05-10 12:31:37 -0500

    fixed code

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
-''' function to add two matrixes'''
+''' function to add two matrixes i dont know how much is enough
+
+
+
+ hopefullu this is enough stupid checker
+'''
 
 
 def add_matrices2D(mat1, mat2):
-   result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
-   return result
+    if len(mat1) == len(mat2):
+        result = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+    return result
+    else:
+        return None
