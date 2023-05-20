b686037b507f36a6de75585c6e496e47685a4fdf
gavintobin <gavintobin714@gmail.com>
2023-05-20 11:51:02 -0500

    try1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,24 +1,27 @@
 #!/usr/bin/env python3
 """class poisson"""
-π = 3.1415926536
+pi = 3.1415926536
 e = 2.7182818285
 
-def __init__(self, data=None, lambtha=1.):
-    """innit"""
-    self.lambtha=float(lambtha)
-    if data == None:
-        if lambtha <= 0:
-            raise ValueError('data must be a positive value')
-        if data:
+class Poisson:
+    '''poisson class'''
+    def __init__(self, data=None, lambtha=1.):
+        """innit"""
+        self.lambtha = float(lambtha)
+        if data  is None:
+            if lambtha <= 0:
+                raise ValueError('data must be a positive value')
+        if data is not None:
             if len(data) < 2:
-            raise ValueError('data must contain multiple values')
+                raise ValueError('data must contain multiple values')
             if type(data) != list:
                 raise TypeError('data must be a list')
-            sum(data) / sum(lambtha)
+            else:
+                self.lambtha = sum(data) / len(data)
 
 
-def cdf(self, k):
+'''def cdf(self, k):
     """calculatew value of the cdf for given number of successess"""
-    if type(k) != int:
+    if type(k) != int:'''
      
         
