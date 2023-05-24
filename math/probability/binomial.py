c2a0a3fc799d96ae2b9f70265405aa7008713455
gavintobin <gavintobin714@gmail.com>
2023-05-24 16:36:38 -0500

    try1

---
+++ math/probability/binomial.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""binomial class"""
+
+pi = 3.1415926536
+e = 2.7182818285
+
+
+class Binomial:
+    """binom class"""
+    def __init__(self, data=None, n=1, p=0.5):
+        """init"""
+        self.n = int(n)
+        self.p = float(p)
+        if data is None:
+            if self.n <= 0:
+                raise ValueError('n must be a positive value')
+            if self.p < 0 or > 1:
+                raise ValueError('p msut be greater than 0 or less than 1')
+        if data is not None:
+            if type(data) != list:
+                raise TypeError('data must be a list')
+            if data < 2:
+                raise ValueError('data must contain multiple values')
+            mean = sum(data) / len(data)
+            variance = 0
+            for number in data:
+                variance = variance + (number - mean) ** 2
+            variance = variance / len(data)
+            q = variance / mean
+            p1 = 1 - q
+            n1 = (sum(data) / p1) / len(data)
+            self.n = int(round(n1))
+            self.p = float(mean/self.n)
+            
+
