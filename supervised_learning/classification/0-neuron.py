3381f86e257e2985a6b786f491b7e0d49e1b2cd8
gavintobin <gavintobin714@gmail.com>
2023-06-02 12:15:35 -0500

    try1

---
+++ supervised_learning/classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''neauron class that defines singleneuron performing binary classification'''
+import numpy as np
+
+
+class Neuron:
+    '''neauron class'''
+    def __init__(self, nx):
+        '''innit'''
+        self.A = 0
+        self.b = 0
+        self.w = np.random.randn(1, nx)
+        if type(nx) != int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
