f725cd98c734f530e10f563b54624569be0f73af
gavintobin <gavintobin714@gmail.com>
2023-06-05 14:07:07 -0500

    try2

--- supervised_learning/classification/8-neural_network.py
+++ supervised_learning/classification/8-neural_network.py
@@ -7,12 +7,6 @@ import numpy as np
 class NeuralNetwork:
     '''class'''
     def __init__(self, nx, nodes):
-        self.W1 = np.random.randn(nodes, nx)
-        self.b1 = np.zeros((nodes, 1))
-        self.A1 = 0
-        self.W2 = np.random.randn(1, nodes)
-        self.b2 = 0
-        self.A2 = 0
         if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
@@ -21,3 +15,9 @@ class NeuralNetwork:
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
+        self.W1 = np.random.randn(nodes, nx)
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.randn(1, nodes)
+        self.b2 = 0
+        self.A2 = 0
