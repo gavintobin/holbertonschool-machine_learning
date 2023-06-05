233fd7a4d0b2c06d06c7ad8477d8b56c5c8c1b09
gavintobin <gavintobin714@gmail.com>
2023-06-05 17:00:41 -0500

    try1

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -9,7 +9,7 @@ class DeepNeuralNetwork:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
-        if type(layers) is not list or len(layers) == 0:
+        if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError('layers must be a list of postive integers')
         if False in (np.array(layers) > 0):
             raise TypeError("layers must be a list of positive integers")
