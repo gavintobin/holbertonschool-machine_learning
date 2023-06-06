0aa8d68adbfb30f5ebd870270edf0954a04f54ff
gavintobin <gavintobin714@gmail.com>
2023-06-06 14:15:50 -0500

    try1

--- supervised_learning/classification/24-one_hot_encode.py
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -7,6 +7,7 @@ def one_hot_encode(Y, classes):
     if not isinstance(Y, np.ndarray) or len(Y) == 0 or not isinstance(classes, int) or classes <= 0:
         return None
     m = Y.shape[0]
+    one_hot = np.zeros((classes, m))
     for i in range(m):
         if Y[i] >= 0:
             one_hot = np.zeros((classes, m))
