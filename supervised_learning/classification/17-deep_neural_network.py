db928cb5d75dbe786522ed79345a236cea709011
gavintobin <gavintobin714@gmail.com>
2023-06-06 10:42:58 -0500

    try1

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""deep neural network w binary classif."""
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''dnn class'''
+    def __init__(self, nx, layers):
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+        if False in (np.array(layers) > 0):
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for i in range(self.__L):
+            if i == 0:
+                w = np.random.randn(layers[i], nx) * np.sqrt(2 / nx)
+                self.__weights['W' + str(i + 1)] = w
+            else:
+                prev = layers[i-1]
+                w = np.random.randn(layers[i], prev) * np.sqrt(2 / prev)
+                self.__weights['W' + str(i + 1)] = w
+            self.__weights['b' + str(i + 1)] = np.zeros((layers[i], 1))
+
+    @property
+    def L(self):
+        """layer getter"""
+        return self.__L
+
+    @property
+    def cache(self):
+        '''itermed val getter'''
+        return self.__cache
+
+    @property
+    def weights(self):
+        '''weight getter'''
+        return self.__weights
