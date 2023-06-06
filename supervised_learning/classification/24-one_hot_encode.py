945e54f5ae7d765cab07c38b42eb22ec6af51e51
gavintobin <gavintobin714@gmail.com>
2023-06-06 14:05:29 -0500

    try1

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''hot encode'''
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """hes on fiyaaaa"""
+    if not isinstance(Y, np.ndarray) or len(Y) == 0 or not isinstance(classes, int) or classes <= 0:
+        return None
+
+    one_hot = np.zeros((classes, len(Y)))
+    one_hot[Y, np.arange(len(Y))] = 1
+
+    return one_hot
