f26b33c8f7a157bc1c67e0d3de9d643712053f51
gavintobin <gavintobin714@gmail.com>
2023-06-12 09:52:55 -0700

    try1

--- supervised_learning/tensorflow/5-create_train_op.py
+++ supervised_learning/tensorflow/5-create_train_op.py
@@ -3,8 +3,8 @@
 
 import tensorflow as tf
 
+
 def create_trian_op(loss, alpha):
     '''love it'''
-    optimizer = tf.train.GradientDescentOptimizer(learning_rate=alpha)
-    train_op = optimizer.minimize(loss)
-    return train_op
+    opty = tf.train.GradientDescentOptimizer(alpha).minimise(loss)
+    return opty
