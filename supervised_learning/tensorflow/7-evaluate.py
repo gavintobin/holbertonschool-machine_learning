0f291387acc38f298f620b46557ac7ef5db4d074
gavintobin <gavintobin714@gmail.com>
2023-06-12 10:33:54 -0700

    try1

--- supervised_learning/tensorflow/7-evaluate.py
+++ supervised_learning/tensorflow/7-evaluate.py
@@ -9,9 +9,9 @@ def evaluate(X, Y, save_path):
     saved = tf.train.import_meta_graph(save_path + '.meta')
     saved.restore(sess, save_path)
     graph = tf.get_default_graph()
-    x = graph.get_tensor_by_name('x')[0]
-    y = graph.get_tensor_by_name('y')[0]
-    y_pred = graph.get_collection('tensors')[0]
+    x = graph.get_collection('x')[0]
+    y = graph.get_collection('y')[0]
+    y_pred = graph.get_collection('y_pred')[0]
     loss = graph.get_collection('loss')[0]
     accuracy = graph.get_collection('accuracy')[0]
     return tuple(sess.run([y_pred, accuracy, loss], feed_dict={x: X, y: Y}))
