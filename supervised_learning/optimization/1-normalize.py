2bb1573777c1cc75c42f8ded9c81698ce545ea86
gavintobin <gavintobin714@gmail.com>
2023-06-16 16:43:48 -0500

    try1

---
+++ supervised_learning/optimization/1-normalize.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''task 2'''
+
+import numpy as np
+
+
+def normalize(X, m, s):
+    '''calcs normalized in whole  matrix'''
+    normalized = (normalized - m) / s
+    return normalized
