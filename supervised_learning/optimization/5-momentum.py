28af0a55753c800dfb4f64eec1d501209ab16219
gavintobin <gavintobin714@gmail.com>
2023-06-20 12:32:15 -0500

    try1

--- supervised_learning/optimization/5-momentum.py
+++ supervised_learning/optimization/5-momentum.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 def update_variables_momentum(alpha, beta1, var, grad, v):
-    '''updates vaiables w gd'''
-    um = beta1 + v + (grad * (1 - beta1))
-    uv = var- (alpha * um)
+    '''updates vaiables gd'''
+    um = beta1 * v + (grad * (1 - beta1))
+    uv = var - (alpha * um)
     return uv, um
