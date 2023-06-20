db4de806d86d0e3b9f14c620125b84af7156c6c8
gavintobin <gavintobin714@gmail.com>
2023-06-20 12:40:01 -0500

    try1

--- supervised_learning/optimization/6-momentum.py
+++ supervised_learning/optimization/6-momentum.py
@@ -2,9 +2,7 @@
 '''task 7'''
 import tensorflow as tf
 
+
 def create_momentum_op(loss, alpha, beta1):
     ''' create train op for nn'''
-    optimizer = tf.train.MomentumOptimizer(learning_rate=alpha, momentum=beta1)
-    train_op = tf.train.optimizer.minimum(loss)
-    return train_op
-
+    return tf.train.MomentumOptimizer(alpha, beta1).minimize(loss)
