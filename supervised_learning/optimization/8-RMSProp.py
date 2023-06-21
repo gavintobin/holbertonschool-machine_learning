9af7efa2c68e9f676524dc21e20fa3b4dfbb603c
gavintobin <gavintobin714@gmail.com>
2023-06-21 14:20:00 -0500

    try1

--- supervised_learning/optimization/8-RMSProp.py
+++ supervised_learning/optimization/8-RMSProp.py
@@ -6,4 +6,4 @@ import tensorflow as tf
 
 def create_RMSProp_op(loss, alpha, beta2, epsilon):
     '''rms optimo'''
-    return tf.train.RMSpropOptimizer(alpha, beta2, epsilon).minimise(loss)
+    return tf.train.RMSpropOptimizer(alpha, beta2, epsilon).minimize(loss)
