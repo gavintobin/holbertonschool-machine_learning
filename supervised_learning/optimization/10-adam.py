7954040c6d18b82a2db12c5d940d8e535b165988
gavintobin <gavintobin714@gmail.com>
2023-06-21 15:07:05 -0500

    try1

--- supervised_learning/optimization/10-adam.py
+++ supervised_learning/optimization/10-adam.py
@@ -6,3 +6,4 @@ import tensorflow as tf
 def update_variables_Adam(loss, alpha, beta1, beta2, epsilon):
     '''adam opt to update vars'''
     return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon).minimizer(loss)
+
