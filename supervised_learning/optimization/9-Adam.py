397baa8328596f1d0730eb69de289ca89a342b52
gavintobin <gavintobin714@gmail.com>
2023-06-21 15:13:06 -0500

    try1

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3 
 '''task 10'''
-import tensorflow as tf
+import numpy as np
 
 
-def update_variables_Adam(alpha, beta1,
-                          beta2, epsilon, var, grad, v, s, t):
-    '''adam opt to update vars'''
-    return tf.train.AdamOptimizer(alpha, beta1, beta2, epsilon,
-                                   var, grad, )
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """Update Variables For Adam"""
+    v = ((beta1 * v) + ((1 - beta1) * grad))
+    s = ((beta2 * s) + ((1 - beta2) * grad ** 2))
+    newV = (v / (1 - beta1 ** t))
+    newS = (s / (1 - beta2 ** t))
+    var = var - alpha * newV / (np.sqrt(newS) + epsilon)
+
+    return var, v, s
