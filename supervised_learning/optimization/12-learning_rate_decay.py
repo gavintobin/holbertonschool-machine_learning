587672c3f0ab1c145896ed641fdfab22d794c0bc
gavintobin <gavintobin714@gmail.com>
2023-06-22 11:22:08 -0500

    try1

--- supervised_learning/optimization/12-learning_rate_decay.py
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -6,4 +6,6 @@ import tensorflow as tf
 
 def learning_rate_decay(alpha, decay_rate, global_step, decay_step):
     '''inverse time decary to calc lrd'''
-    return tf.train.inverse_time_decay(alpha, global_step, decay_step, decay_rate, stair_case=True)
+    return tf.train.inverse_time_decay(learning_rate=alpha, global_step=global_step,
+                                       decay_steps=decay_step,
+                                       decay_rate=decay_rate, stair_case=True)
