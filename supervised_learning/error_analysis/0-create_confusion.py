ea4fab9db3a7bef0714bd566a88966e5990db016
gavintobin <gavintobin714@gmail.com>
2023-06-26 10:57:08 -0500

    try1

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -5,4 +5,19 @@ import numpy as np
 
 def  create_confusion_matrix(labels, logits):
     '''creates a confusion matrix'''
-    
+    m, classes = labels.shape
+
+
+    true_labels = np.argmax(labels, axis=1)
+    predicted_labels = np.argmax(logits, axis=1)
+
+
+    confusion = np.zeros((classes, classes), dtype=np.int32)
+
+
+    for i in range(m):
+        true_label = true_labels[i]
+        predicted_label = predicted_labels[i]
+        confusion[true_label, predicted_label] += 1
+
+    return confusion
