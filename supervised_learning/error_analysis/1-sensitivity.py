7bae74be0e20bcd6259cf428f0c6da5695a4f86e
gavintobin <gavintobin714@gmail.com>
2023-06-26 11:30:43 -0500

    try1

---
+++ supervised_learning/error_analysis/1-sensitivity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''task 2'''
+import numpy as np
+
+def sesnsitivity(confusion):
+    '''calcs sens of each class'''
+    classes = confusion.shape[0]
+    sensitivity_values = np.zeros(classes, dtype=np.float32)
+
+    for i in range(classes):
+        true_positives = confusion[i, i]
+        actual_positives = np.sum(confusion[i, :])
+        sensitivity_values[i] = true_positives / actual_positives
+
+    return sensitivity_values
