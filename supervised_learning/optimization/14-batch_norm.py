e98ca3313cded7d5b86daeedea81c9a8cfedddfe
gavintobin <gavintobin714@gmail.com>
2023-06-26 10:31:24 -0500

    try1

--- supervised_learning/optimization/14-batch_norm.py
+++ supervised_learning/optimization/14-batch_norm.py
@@ -6,13 +6,13 @@ import tensorflow as tf
 def create_batch_norm_layer(prev, n, activation):
     '''same but in tf'''
     kerninit = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    layers = tf.layers.dense(prev, n, kerninit)
+    l = tf.layers.dense(prev, n, kerninit)
 
-    mean, var = tf.nn.moments(layers, 0)
+    mean, var = tf.nn.moments(l, 0)
 
     gamma = tf.Variable(tf.ones[n])
     beta = tf.Variable(tf.zeros[n])
 
     eps = 1 * (10 ** -8)
-    normz = tf.nn.batch_normalization(layers, mean, var, beta, gamma, eps)
+    normz = tf.nn.batch_normalization(l, mean, var, beta, gamma, eps)
     return activation(normz)
