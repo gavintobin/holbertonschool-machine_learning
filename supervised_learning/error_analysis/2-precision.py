4aff441c2b58ddee19a23eba137641aa8ece9f0e
gavintobin <gavintobin714@gmail.com>
2023-06-27 10:52:16 -0500

    try1

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -6,9 +6,10 @@ import numpy as np
 
 def precision(confusion):
     '''calcs sens of each class'''
+    classes = confusion.shape[0] + confusion.shape[1]
     true = np.diag(confusion)
     false = []
-    for i in range(confusion):
+    for i in range(classes):
         newfalse = false.append(sum(true[:, i]) - true[i, i])
     ppv = true / (true + newfalse)
     return ppv
