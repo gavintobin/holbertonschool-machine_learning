978c65d4d06ceab8f300b61156bac8cfb965e217
gavintobin <gavintobin714@gmail.com>
2023-06-27 10:20:16 -0500

    try1

--- supervised_learning/error_analysis/2-precision.py
+++ supervised_learning/error_analysis/2-precision.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def precision(confusion):
     '''calcs sens of each class'''
-    truepos = np.sum(confusion, axis=0)
-    falsepos = np.sum(confusion, axis=1)
+    truepos = np.sum(confusion, axis=1)
+    falsepos = np.sum(confusion, axis=0)
     score = truepos / (falsepos + truepos)
     return score
