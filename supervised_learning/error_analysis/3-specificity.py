93888c6572722c83d25da1e970eccb1d46e5beff
gavintobin <gavintobin714@gmail.com>
2023-06-27 11:20:36 -0500

    try1

--- supervised_learning/error_analysis/3-specificity.py
+++ supervised_learning/error_analysis/3-specificity.py
@@ -5,7 +5,10 @@ import numpy as np
 
 def specificity(confusion):
     '''calcs spec for each class'''
-    truepos = np.diag(confusion)
-    falseneg = confusion.sum(axis=1) - truepos
-    spec = truepos / (truepos + falseneg)
+    FP = confusion.sum (axis=0) - TP
+    FN = confusion.sum (axis=1) - TP
+    TP = np.diag (confusion)
+    TN = confusion.values.sum () - (FP + FN + TP)
+    spec = TN / (TN + FP)
+
     return spec
