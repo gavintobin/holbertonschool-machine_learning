ea0206482a7a9e2f1da48d4025be4a9e00fa15e4
gavintobin <gavintobin714@gmail.com>
2023-06-27 11:41:26 -0500

    try1

--- supervised_learning/error_analysis/4-f1_score.py
+++ supervised_learning/error_analysis/4-f1_score.py
@@ -5,11 +5,9 @@ sensitivity = __import__('1-sensitivity').sensitivity
 precision = __import__('2-precision').precision
 
 
-
 def f1_score(confusion):
     '''calcs f1 score'''
     numer = sensitivity(confusion) * precision(confusion)
     denom = sensitivity(confusion) + precision(confusion)
     f1 = 2 * (numer / denom)
     return f1
-
