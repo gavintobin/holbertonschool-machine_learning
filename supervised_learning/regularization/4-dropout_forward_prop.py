f966a0c03172b437b67e0f41c5d39946d5093986
gavintobin <gavintobin714@gmail.com>
2023-07-03 09:15:25 -0500

    try1

--- supervised_learning/regularization/4-dropout_forward_prop.py
+++ supervised_learning/regularization/4-dropout_forward_prop.py
@@ -7,22 +7,21 @@ def dropout_forward_prop(X, weights, L, keep_prob):
     '''does fp using dropout'''
     cache = {}
     cache['A0'] = X
+    A_prev = cache['A' + str(l - 1)]
     
-    for l in range(1, L + 1):
-        A_prev = cache['A' + str(l - 1)]
-        W = weights['W' + str(l)]
-        b = weights['b' + str(l)]
-        Z = np.dot(W, A_prev) + b
-        
-        if l < L:
-            D = np.random.rand(Z.shape[0], Z.shape[1])
-            D = D < keep_prob
-            A = np.multiply(np.tanh(Z), D) / keep_prob
-            cache['D' + str(l)] = D
+    for i in range(1, L + 1):
+        W = weights['W' + str(i)]
+        b = weights['b' + str(i)]
+        Z = np.matmul(W, A_prev) + b
+
+        if i == L:
+            A = np.exp(Z) / np.sum(np.exp(Z), axis=0, keepdims=True)
         else:
-            A = np.exp(Z) / np.sum(np.exp(Z), axis=0)
-        
-        cache['Z' + str(l)] = Z
-        cache['A' + str(l)] = A
-    
+            A = np.tanh(Z)
+            dropout = np.random.binomial(n=1, p=keep_prob, size=A.shape)
+            A = A * dropout / keep_prob
+            cache['D' + str(i)] = dropout
+
+        cache['A' + str(i)] = A
+
     return cache
