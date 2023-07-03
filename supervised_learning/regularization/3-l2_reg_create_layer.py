12b76e9b8083732a18b91483b10c2eb8f4055e7f
gavintobin <gavintobin714@gmail.com>
2023-07-03 08:58:44 -0500

    try1

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -7,6 +7,6 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     ''' creates tf layer w l2 reg'''
     regularizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
     
-    layer = tf.layers.dense(inputs=prev, units=n, activation=activation, kernel_regularizer=regularizer)
+    layer = tf.layers.dense(inputs=prev, units=n, activation=activation,kernal_initializer=None, kernel_regularizer=regularizer)
     
     return layer(prev)
