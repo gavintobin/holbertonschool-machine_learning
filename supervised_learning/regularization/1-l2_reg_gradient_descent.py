6612c255a6699f3718920a98d29986cab3aeb6ef
gavintobin <gavintobin714@gmail.com>
2023-07-03 08:31:27 -0500

    try1

--- supervised_learning/regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/regularization/1-l2_reg_gradient_descent.py
@@ -6,14 +6,24 @@ import numpy as np
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     '''updates w and b using gd w l2 regularization'''
     m = Y.shape[1]
-    dz = cache['A' + str(L)] - Y
     for i in range(L, 0, -1):
-        W = weights["W" + str(i)]
-        aprev = cache['A' + str(i - 1)]
-        dw = (1 / m) + np.dot(dz, aprev.T) + (lambtha / m) * W
-        db = (1 / m) * np.sum(dz, axis=1, keepdims=True)
-        if i > 1:
-            da = np.dot(W.T, dz)
-            dz = da + (1 - cache['A' + str(i - 1)]**2)
-        W -= alpha * dw
+        A = cache['A' + str(i)]
+        A_prev = cache['A' + str(i - 1)]
+
+        if i == L:
+            dZ = A - Y
+        else:
+            dZ = dA * (1 - np.power(A, 2))
+
+        W = weights['W' + str(i)]
+
+        dA = np.matmul(W.T, dZ)
+
+        j = ((lambtha / m) * W)
+        dW = (1 / m) * np.matmul(dZ, A_prev.T) + j
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+
+        weights['W' + str(i)] -= alpha * dW
         weights['b' + str(i)] -= alpha * db
+
+    return weights
