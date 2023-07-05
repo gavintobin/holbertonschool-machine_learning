e98bd5805de521da7f510fbc241c2a888255da8e
gavintobin <gavintobin714@gmail.com>
2023-07-05 12:02:40 -0500

    spelling

---
+++ supervised_learning/keras/1-input.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''task 1'''
+import tensorflow.keras as K
+
+
+def build_model(nx, layers, activations, lambtha, keep_prob):
+    '''same as first task?'''
+    model = K.Sequential()
+
+    model.add(K.layers.InputLayer(input_shape=(nx, )))
+
+    for i in range(len(layers)):
+        model.add(K.layers.Dense(units=layers[i],
+                                 activation=activations[i],
+                                 kernel_regularizer=K.regularizers.l2(lambtha)))
+
+        if i < len(layers) - 1:
+            model.add(K.layers.Dropout(1 - keep_prob))
+    return model
