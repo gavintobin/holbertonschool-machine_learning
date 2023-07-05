aa65bcf8a63ca052c0860385e0a40061946757a4
gavintobin <gavintobin714@gmail.com>
2023-07-05 11:25:16 -0500

    try1

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def l2_reg_create_layer(prev, n, activation, lambtha):
     ''' creates tf layer w l2 reg'''
     regularizer = tf.contrib.layers.l2_regularizer(scale=lambtha)
-    
-    layer = tf.layers.dense(inputs=prev, units=n, activation=activation,kernal_initializer=None, kernel_regularizer=regularizer)
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(inputs=prev, units=n, activation=activation,kernal_initializer=init, kernel_regularizer=regularizer)
     
     return layer(prev)
