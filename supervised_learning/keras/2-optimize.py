0b930f65e7e966b4c367de679036f1f852df6111
gavintobin <gavintobin714@gmail.com>
2023-07-06 10:31:06 -0500

    try1

--- supervised_learning/keras/2-optimize.py
+++ supervised_learning/keras/2-optimize.py
@@ -5,4 +5,7 @@ import tensorflow.keras as K
 
 def optimize_model(network, alpha, beta1, beta2):
     '''adam op w cross entro loss '''
-    
+    model = network.compile(optimizer=K.optimizers.Adam(alpha, beta1, beta2),
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
+    return model
