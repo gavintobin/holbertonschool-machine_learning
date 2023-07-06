0aa747141ea3f1df4d6f2dd2f04739beaf226f81
gavintobin <gavintobin714@gmail.com>
2023-07-06 09:53:20 -0500

    try1

--- supervised_learning/keras/4-train.py
+++ supervised_learning/keras/4-train.py
@@ -3,7 +3,7 @@
 import tensorflow.keras as K
 
 
-def train_model(network, data, labels, batch_size, epochs,verbose=True,
+def train_model(network, data, labels, batch_size, epochs, verbose=True,
                 shuffles=False):
     '''train da model'''
     model = network.fit(x=data, y=labels, batch_size=batch_size,
