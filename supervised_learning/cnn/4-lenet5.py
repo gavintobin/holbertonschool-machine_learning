036e5faeb2bf0c10042fd9961bdd298d2fe52862
gavintobin <gavintobin714@gmail.com>
2023-07-12 09:58:58 -0500

    try1

---
+++ supervised_learning/cnn/4-lenet5.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+'''task 4'''
+import tensorflow as tf
+
+
+def lenet5(x, y):
+    '''builds modded fersion of lenet5 architechure using tf'''
+    he_init = tf.contrib.layers.variance_scaling_initializer()
+
+    first_conv = tf.layers.conv2d(x, filters=6,
+                                  kernal_size=(5, 5), padding='same',
+                                  activation=tf.nn.relu,
+                                  kernel_initializer=he_init)
+    first_pool = tf.layers.max_pooling2d(first_conv, pool_size=(2, 2),
+                                         strides=(2, 2))
+    sec_conv = tf.layers.cov2d(first_pool, filters=16,
+                               kernel_size=(5,5), padding='valid', 
+                               activation=tf.nn.relu, 
+                               kernel_initializer=he_init)
+    sec_pool = tf.layers.max_pooling2d(sec_conv, pool_size=(2, 2),
+                                       strides=(2, 2))
+    flat = tf.layers.flatten(sec_pool)
+
+    firstfull = tf.layers.Dense(flat, units=120, activation=tf.nn.relu,
+                                kernel_initializer=he_init)
+    secfull = tf.layers.Dense(firstfull, units=84, activation=tf.nn.relu,
+                              kernel_initalizer=he_init)
+    logits = tf.layers.Dense(secfull, units=10, kernel_initializer=he_init)
+    output = tf.nn.softmax(logits)
+    loss = tf.losses.softmax_cross_entropy(onehot_labels=y,
+                                           logits=logits)
+    optimizer = tf.train.AdamOptimizer
+    trainop = optimizer.moinimize(loss)
+
+    corpred = tf.equal(tf.argmax(output, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(corpred, tf.float32))
+
+    return output, trainop, loss, accuracy
