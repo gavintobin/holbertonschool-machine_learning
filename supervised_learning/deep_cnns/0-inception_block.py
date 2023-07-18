e0cf7d5a64d9c1ead76c5289e128e35f409a5159
gavintobin <gavintobin714@gmail.com>
2023-07-18 09:35:38 -0500

    try1

--- supervised_learning/deep_cnns/0-inception_block.py
+++ supervised_learning/deep_cnns/0-inception_block.py
@@ -8,17 +8,17 @@ def inception_block(A_prev, filters):
     F1, F3R, F3, F5R, F5, FPP = filters
     conv_1by = K.layers.Conv2D(filters=F1, kernel_size=(1, 1),
                                activation='relu')(A_prev)
-    conv_1byb4 = K.layers.Conv2d(filters=F3R, kernel_size=(1, 1),
+    conv_1byb4 = K.layers.Conv2D(filters=F3R, kernel_size=(1, 1),
                                  activation='relu')(A_prev)
-    conv_3x3 = K.layers.Conv2d(filters=F3, kernel_size=(3, 3),
+    conv_3x3 = K.layers.Conv2D(filters=F3, kernel_size=(3, 3),
                                activation='relu')(conv_1byb4)
-    conv_5x5b4 = K.layers.Conv2d(filters=F5R, kernel_size=(1, 1),
+    conv_5x5b4 = K.layers.Conv2D(filters=F5R, kernel_size=(1, 1),
                                  activation='relu')(A_prev)
-    conv_5x5 = K.layers.Conv2d(filters=F5, kernel_size=(5, 5),
+    conv_5x5 = K.layers.Conv2D(filters=F5, kernel_size=(5, 5),
                                activation='relu')(conv_5x5b4)
-    pool = K.layers.MaxPool2d(pool_size=(3, 3),
+    pool = K.layers.MaxPool2D(pool_size=(3, 3),
                               strides=(1, 1), padding='same')(A_prev)
-    pool_1x1 = K.layers.Conv2d(filters=FPP, activation='relu')(pool)
+    pool_1x1 = K.layers.Conv2D(filters=FPP, activation='relu')(pool)
 
     output = K.layers.concatenate([conv_1by,
                                    conv_3x3,
