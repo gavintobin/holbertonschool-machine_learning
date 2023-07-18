39aa93f35f35e7e39579b225fb683ab331b38101
gavintobin <gavintobin714@gmail.com>
2023-07-18 13:22:36 -0500

    try1

---
+++ supervised_learning/deep_cnns/6-transition_layer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''task2'''
+import tensorflow.keras as K
+
+
+def transition_layer(X, nb_filters, compression):
+    '''buils transition layer'''
+    init = K.initializers.he_normal()
+    filters = (int)(nb_filters * compression)
+
+    firstbatch = K.layers.BatchNormalization(axis=3)(X)
+    firstrelu = K.layers.ReLU()(firstbatch)
+    firstconv = K.layers.Conv2D(filters=filters,
+                           kernel_size=(1, 1),
+                           padding='same',
+                           kernel_initializer=init)(firstrelu)
+
+    pool =  K.layers.AveragePooling2D(pool_size=(2, 2),
+                                        strides=2,
+                                        padding='same')(firstconv)
+    return (pool, filters)
