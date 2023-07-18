9dcb35f6054773257d03f1477eecf4af208489d6
gavintobin <gavintobin714@gmail.com>
2023-07-18 13:10:29 -0500

    try1

---
+++ supervised_learning/deep_cnns/5-dense_block.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+'''task2'''
+import tensorflow.keras as K
+
+
+def dense_block(X, nb_filters, growth_rate, layers):
+    '''builds dense block'''
+
+    init = K.initializers.he_normal()
+
+    concatenation = X
+    for i in range(layers):
+        firstbatch = K.layers.BatchNormalization(axis=3)(concatenation)
+        firstrelu = K.layers.ReLU()(firstbatch)
+        firstconv = K.layers.Conv2D(filters=(4 * growth_rate),
+                                    kernel_size=(1, 1),
+                                    padding='same',
+                                    kernel_initializer=init)(firstrelu)
+
+        secbatch = K.layers.BatchNormalization(axis=3)(firstconv)
+        secrelu = K.layers.ReLU()(secbatch)
+        seconv = K.layers.Conv2D(filters=growth_rate,
+                                 kernel_size=(3, 3),
+                                 padding='same',
+                                 kernel_initializer=init)(secrelu)
+        concatenation = K.layers.Concatenate()([concatenation, seconv])
+        nb_filters = nb_filters + growth_rate
+
+    return (concatenation, nb_filters)
