13f1cb819c40fe62dbd59ed9eb57e040b535a325
gavintobin <gavintobin714@gmail.com>
2023-08-23 08:01:58 -0500

    try1

--- supervised_learning/object_detection/2-yolo.py
+++ supervised_learning/object_detection/2-yolo.py
@@ -57,30 +57,30 @@ class Yolo():
 
         return boxes, box_confidences, box_class_probs
 
-    def filter_boxes(self, boxes, box_confidences, box_class_probs):
-        '''filter boxes'''
-        filtered_boxes, box_classes, box_scores = [], [], []
+       def filter_boxes(self, boxes, box_confidences, box_class_probs):
+            '''filter boxes'''
+            filtered_boxes, box_classes, box_scores = [], [], []
 
-        for box, confidence, class_probs in zip(boxes, box_confidences, box_class_probs):
-            # Calculate box scores by multiplying box_confidence and class probabilities
-            scores = confidence * class_probs
+            for box, confidence, class_probs in zip(boxes, box_confidences, box_class_probs):
+                # Calculate box scores by multiplying box_confidence and class probabilities
+                scores = confidence * class_probs
 
-            # Find indices of class predictions that exceed class threshold
-            class_indices = np.argmax(scores, axis=-1)
-            class_scores = np.max(scores, axis=-1)
+                # Find indices of class predictions that exceed class threshold
+                class_indices = np.argmax(scores, axis=-1)
+                class_scores = np.max(scores, axis=-1)
 
-            # Filter out boxes with scores below box threshold
-            mask = class_scores >= self.class_threshold
-            filtered_boxes.extend(box[mask])
-            box_classes.extend(class_indices[mask])
-            box_scores.extend(class_scores[mask])
+                # Filter out boxes with scores below box threshold
+                mask = class_scores >= self.class_threshold
+                filtered_boxes.extend(box[mask])
+                box_classes.extend(class_indices[mask])
+                box_scores.extend(class_scores[mask])
 
-        filtered_boxes = np.array(filtered_boxes)
-        box_classes = np.array(box_classes)
+            filtered_boxes = np.array(filtered_boxes)
+            box_classes = np.array(box_classes)
 
-        box_scores = np.array(box_scores)
+            box_scores = np.array(box_scores)
 
-        return filtered_boxes, box_classes, box_scores
+            return filtered_boxes, box_classes, box_scores
 
 
 
