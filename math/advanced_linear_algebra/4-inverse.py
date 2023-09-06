82ac11e9ccce7df609124959fcc4fa76acc630c0
gavintobin <gavintobin714@gmail.com>
2023-09-06 09:47:36 -0500

    try1

--- math/advanced_linear_algebra/4-inverse.py
+++ math/advanced_linear_algebra/4-inverse.py
@@ -67,7 +67,7 @@ def determinant(matrix):
 
 
 def inverse(matrix):
-    # Check if matrix is a list of lists
+    '''inverse'''
     if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix):
         raise TypeError("matrix must be a list of lists")
 
