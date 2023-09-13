11ee8f89170160ac56f2a77eeb015fbc6e55ff69
gavintobin <gavintobin714@gmail.com>
2023-09-13 11:36:00 -0500

    try1

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 '''task 3'''
 import numpy as np
+import scipy.linalg
 
 
 class MultiNormal:
@@ -23,6 +24,11 @@ class MultiNormal:
         # Calculate the covariance matrix
         self.cov = self.calculate_covariance(data)
 
+        try:
+            self.L = scipy.linalg.cholesky(self.cov, lower=True)
+        except np.linalg.LinAlgError:
+            raise ValueError("Covariance matrix is not positive definite.")
+
     def calculate_covariance(self, data):
         '''calc koe'''
         d, n = data.shape
@@ -37,3 +43,22 @@ class MultiNormal:
                 cov[i, j] = np.dot(xi - self.mean[i], xj - self.mean[j]) / nn
 
         return cov
+    def pdf(self, x):
+        # Check if x is a numpy.ndarray
+        if not isinstance(x, np.ndarray):
+            raise TypeError("x must be a numpy.ndarray")
+
+        # Check if x has the correct shape (d, 1)
+        d, _ = self.mean.shape
+        if x.shape != (d, 1):
+            raise ValueError(f"x must have the shape ({d}, 1)")
+
+        # Calculate the Mahalanobis distance
+        diff = x - self.mean
+        manlob_dist = np.linalg.solve(self.L, diff)
+        manlob_dist = np.dot(manlob_dist.T, manlob_dist)
+
+        # Calculate the PDF
+        pdf = np.exp(-0.5 * manlob_dist) / (np.sqrt((2 * np.pi) ** d * np.linalg.det(self.cov)))
+
+        return pdf
