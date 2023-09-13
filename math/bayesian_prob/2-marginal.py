506f05c9f0e84fe8d38efb970c4df9e87fbe9c78
gavintobin <gavintobin714@gmail.com>
2023-09-13 12:49:40 -0500

    try1

--- math/bayesian_prob/2-marginal.py
+++ math/bayesian_prob/2-marginal.py
@@ -15,24 +15,21 @@ def marginal(x, n, P, Pr):
 
     if x > n:
         raise ValueError('x cannot be greater than n')
-    # Check if P is a 1D numpy.ndarray
-    if not isinstance(P, np.ndarray) or P.ndim != 1:
-        raise TypeError("P must be a 1D numpy.ndarray")
 
-    # Check if Pr is a numpy.ndarray with the same shape as P
+    if not isinstance(P, np.ndarray) or len(P.shape) != 1:
+        raise TypeError('P must be a 1D numpy.ndarray')
+
     if not isinstance(Pr, np.ndarray) or Pr.shape != P.shape:
-        raise TypeError("Pr must be a numpy.ndarray with the same shape as P")
+        raise TypeError('Pr must be a numpy.ndarray with the same shape as P')
 
-    # Check if all values in P and Pr are in the range [0, 1]
-    if (P < 0).any() or (P > 1).any():
-        raise ValueError(f"All values in P must be in the range [0, 1]")
+    if np.any(P < 0) or np.any(P > 1):
+        raise ValueError('All values in P must be in the range [0, 1]')
 
-    if (Pr < 0).any() or (Pr > 1).any():
-        raise ValueError(f"All values in Pr must be in the range [0, 1]")
+    if np.any(Pr < 0) or np.any(Pr > 1):
+        raise ValueError('All values in Pr must be in the range [0, 1]')
 
-    # Check if Pr sums to 1 (within a small tolerance)
-    if not np.isclose(np.sum(Pr), 1.0):
-        raise ValueError("Pr must sum to 1")
+    if not np.isclose(sum(Pr), 1):
+        raise ValueError('Pr must sum to 1')
 
     # Calculate the marginal probability using the factorial-based method
     marginal_probability = np.sum(Pr * (np.math.factorial(n) / (np.math.factorial(x) * np.math.factorial(n - x)) * (P ** x) * ((1 - P) ** (n - x))))
