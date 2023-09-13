2e937b136c6d0fecfc6ac25f9ea7c8e785e5ac2f
gavintobin <gavintobin714@gmail.com>
2023-09-13 11:28:44 -0500

    try1

--- math/multivariate_prob/multinormal.py
+++ math/multivariate_prob/multinormal.py
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
-'''task 1'''
+'''task 3'''
 import numpy as np
+
+
+class MultiNormal:
+    def __init__(self, data):
+        # Check if data is a 2D numpy.ndarray
+        if not isinstance(data, np.ndarray) or data.ndim != 2:
+            raise TypeError("data must be a 2D numpy.ndarray")
+
+        # Get the shape of the array
+        d, n = data.shape
+
+        # Check if there are multiple data points
+        if n < 2:
+            raise ValueError("data must contain multiple data points")
+
+        # Calculate the mean
+        self.mean = np.mean(data, axis=1, keepdims=True)
+
+        # Calculate the covariance matrix
+        self.cov = self.cov(data)
+
 
 def mean_cov(X):
     '''calc mean and covar '''
@@ -25,6 +46,3 @@ def mean_cov(X):
         cov += np.dot(deviation.T, deviation)
 
     cov /= (n - 1)
-
-    return mean, cov
-
