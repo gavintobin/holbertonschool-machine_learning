35faaaf700e64299d430858cdcaeec807bc68605
gavintobin <gavintobin714@gmail.com>
2023-09-19 10:49:37 -0500

    try1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -6,23 +6,30 @@ import numpy as np
 def pca(X, var=0.95):
     ''' performs PCA on dataset'''
     n, d = X.shape
+
     #conv. mat to covar mat
     covarmat = cov(X)
     #compute evalsk and evectss
+
     evall, evect = np.linalg.eigh(covarmat)
     #sort them in desc order
+    
     indicessorted = np.argsort(evall)[ : :-1]
     evalls = evalls[indicessorted]
     evect = evect[:, indicessorted]
+
     #calc explained var
     vari = np.sum(evalls)
     varratio = evalls / vari
+
     #determine which to keep
     cumivar = np.cumsum(varratio)
     keep = np.argmax(cumivar >= var) + 1
+
     #most important components
     mostimp = evect[:, :keep]
     #weights mat
+
     W = mostimp
     return W
 
