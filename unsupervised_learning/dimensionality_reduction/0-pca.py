c115525e854f52fec07b43c9140462c3eddf6e03
gavintobin <gavintobin714@gmail.com>
2023-09-20 12:01:35 -0500

    try1

--- unsupervised_learning/dimensionality_reduction/0-pca.py
+++ unsupervised_learning/dimensionality_reduction/0-pca.py
@@ -4,37 +4,15 @@ import numpy as np
 
 def pca(X, var=0.95):
     ''' performs PCA on dataset'''
-    centeredata = X
+    #perform svd
+    u, s, vt = np.linalg.svd(X, full_matrices=False)
+    explained_variance_ratio = s / np.sum(s)
+    cumulative_variance_ratio = np.cumsum(explained_variance_ratio)
 
-    # Step 2: Compute SVD
-    _, S, Vt = np.linalg.svd(centeredata, full_matrices=False)
+    # which ones to keep
+    num_components_to_keep = np.argmax(cumulative_variance_ratio >= var) + 1
 
-    # Step 3: Calculate explained variance
-    explained_variance_ratio = (S ** 2) / np.sum(S ** 2)
-
-    # Step 4: Determine the number of components to keep
-    cumulative_variance = np.cumsum(explained_variance_ratio)
-    num_components_to_keep = np.argmax(cumulative_variance >= var) + 1
-
-    # Step 5: Select the top principal components
-    top_singular_vectors = Vt[:num_components_to_keep]
-
-    # Step 6: Create the weights matrix W
-    W = top_singular_vectors.T  # Transpose to get (d, nd)
-
+    # Reduce dimensions and get w matrix
+    W = u[:, :num_components_to_keep]
 
     return W
-
-
-'''def cov(x):
-    covariance helper func
-    n, d = x.shape
-    mean = np.mean(x, axis=0)  # Calculate the mean along each dimension
-
-    # Subtract the mean from each data point
-    centered_data = x - mean
-
-    # Compute the covariance matrix
-    cov_matrix = np.dot(centered_data.T, centered_data) / (n - 1)
-
-    return cov_matrix'''
