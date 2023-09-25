158fe90549f398727a82d6c37fe309a9d425b0af
gavintobin <gavintobin714@gmail.com>
2023-09-25 09:01:16 -0500

    try1

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -6,9 +6,11 @@ import numpy as np
 
 def initialize(X, k):
     '''initializes centroid or  centr of cluster'''
-    n, d = X.shape
-    min = np.min(d)
-    max = np.max(d)
+    if k <= 0 or k > X.shape[0]:
+        return None
 
-    centroids = np.random.uniform(low=min, high=max, size=k)
+    min = np.min(X, axis=0)
+    max = np.max(X, axis=0)
+
+    centroids = np.random.uniform(low=min, high=max, size=(k, X.shape[1]))
     return centroids
