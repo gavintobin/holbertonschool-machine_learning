43ce971522c37ee965cdd3a5784be24bd7339036
gavintobin <gavintobin714@gmail.com>
2023-09-26 12:06:12 -0500

    try1

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -22,13 +22,13 @@ def kmeans(X, k, iterations=1000):
         #  figure out  distance between data points and centroids
         distances = np.linalg.norm(X[:, np.newaxis] - centroids, axis=2)
 
-        # Assign each data point to the cluster with the closest centroid
+        # Assign each data point to the cluster  the closest centroid
         clss = np.argmin(distances, axis=1)
 
         # Update cluster centroids based on the mean of assigned data points
         new_C = np.array([X[clss == i].mean(axis=0) for i in range(k)])
 
-        # Handle clusters with no data points by reinitializing their centroids
+        # Handle clusters  no data points by reinitializing their centroids
         empty_clusters = np.isnan(new_C).any(axis=1)
         if np.any(empty_clusters):
             new_C[empty_clusters] = initialize(X, empty_clusters.sum())
