175e4595475183af0462a6bdd3b0e8c8ad228948
gavintobin <gavintobin714@gmail.com>
2023-09-26 12:45:10 -0500

    try1

--- unsupervised_learning/clustering/3-optimum.py
+++ unsupervised_learning/clustering/3-optimum.py
@@ -64,3 +64,19 @@ def initialize(X, k):
 
     centroids = np.random.uniform(low=min, high=max, size=(k, X.shape[1]))
     return centroids
+
+def variance(X, C):
+    '''total intra cluster variance of datas set'''
+    if X.shape[1] != C.shape[1]:
+        return None
+
+    n, d = X.shape
+    k = C.shape[0]
+
+    distances = np.linalg.norm(X[:, np.newaxis] - C, axis=2)**2
+
+    clss = np.argmin(distances, axis=1)
+
+    var = np.sum(distances[np.arange(n), clss])
+
+    return var
