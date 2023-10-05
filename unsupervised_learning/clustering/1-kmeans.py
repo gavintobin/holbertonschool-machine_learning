bb01d5911bacab01986c0ee282dac0cb463a7e28
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 11:45:09 -0500

    Update 1-kmeans.py

--- unsupervised_learning/clustering/1-kmeans.py
+++ unsupervised_learning/clustering/1-kmeans.py
@@ -5,12 +5,14 @@ import numpy as np
 
 def initialize(X, k):
     '''initializes centroid or  centr of cluster'''
+    if type(X) is not np.ndarray or type(k) is not int:
+        return None
+    if k <= 0 or k >= X.shape[0]:
+        return None
 
-    #define minimum an maximum
     min = np.min(X, axis=0)
     max = np.max(X, axis=0)
 
-    #choose random spot  centroid
     centroids = np.random.uniform(low=min, high=max, size=(k, X.shape[1]))
     return centroids
 
