78b49a1a9d1deb926665e08f6e8837a2e4f341a0
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 14:16:01 -0500

    Update 12-agglomerative.py

--- unsupervised_learning/clustering/12-agglomerative.py
+++ unsupervised_learning/clustering/12-agglomerative.py
@@ -1,1 +1,17 @@
+#!/usr/bin/env python3
+'''task12'''
+import scipy.cluster.hierarchy
+import matplotlib.pyplot as plt
 
+
+def agglomerative(X, dist):
+    '''does aglo clust'''
+    lm = scipy.cluster.hierarchy.linkage(X, method='ward')
+
+    clss = scipy.cluster.hierarchy.fcluster(lm, t=dist, criterion='distance')
+
+    scipy.cluster.hierarchy.dendrogram(lm, color_threshold=dist)
+
+    plt.show()
+
+    return clss
