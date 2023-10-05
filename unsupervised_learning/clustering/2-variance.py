364c2b5c33dbe2393ebece13aaf21194aabac5eb
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 14:57:06 -0500

    Update 2-variance.py

--- unsupervised_learning/clustering/2-variance.py
+++ unsupervised_learning/clustering/2-variance.py
@@ -5,14 +5,15 @@ import numpy as np
 
 def variance(X, C):
     '''total intra cluster variance of datas set'''
-    if X.shape[1] != C.shape[1]:
-        return None
     if type(X) is not np.ndarray or type(C) is not np.array:
         return None
 
     if len(X.shape) != 2 or len(C.shape) != 2:
         return None
 
+    if X.shape[1] != C.shape[1]:
+        return None
+    
     distances = np.linalg.norm(X[:, np.newaxis] - C ** 2, axis=2)
 
     clss = np.argmin(distances, axis=1)
