1d6b577b39b8168b32ea01b679ca06a9acf78e0c
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 11:41:02 -0500

    Update 0-initialize.py

--- unsupervised_learning/clustering/0-initialize.py
+++ unsupervised_learning/clustering/0-initialize.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def initialize(X, k):
     '''initializes centroid or  centr of cluster'''
-    if type(X) is not np.ndarray:
+    if type(X) is not np.ndarray or if type(k) is not int:
         return None
     if k <= 0 or k >= X.shape[0]:
         return None
