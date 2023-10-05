a6e777911f4875931226d2b31f332354e768b109
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 14:10:12 -0500

    Update 11-gmm.py

--- unsupervised_learning/clustering/11-gmm.py
+++ unsupervised_learning/clustering/11-gmm.py
@@ -1,1 +1,20 @@
+#!/usr/bin/env python3
+'''task 3'''
+import sklearn.mixture
 
+
+def gmm(X, k):
+   '''does gmm easy way'''
+    gmm = sklearn.mixture.GaussianMixture(n_components=k)
+
+    gmm.fit(X)
+
+    pie = gmm.weights_
+    m = gmm.means_
+    S = gmm.covariances_
+
+    clss = gmm.predict(X)
+
+    bic = gmm.bic(X)
+
+    return pie, m, S, clss, bic
