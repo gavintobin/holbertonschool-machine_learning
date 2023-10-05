1b68b8faa54e8a6fca0fec3610df0ea093977712
gavintobin <109865712+gavintobin@users.noreply.github.com>
2023-10-05 11:28:36 -0500

    Update 6-baum_welch.py

--- unsupervised_learning/hmm/6-baum_welch.py
+++ unsupervised_learning/hmm/6-baum_welch.py
@@ -4,5 +4,5 @@ import numpy as np
 
 
 def baum_welch(Observations, Transition, Emission, Initial, iterations=1000):
-  '''task 6 baum wlwcvh'''
-  return None, None
+    '''task 6 baum wlwcvh'''
+    return None, None
