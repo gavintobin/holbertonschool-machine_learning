922a77180c30ef02aaac7ebd57c4598a7e5bff34
gavintobin <gavintobin714@gmail.com>
2023-10-22 13:34:46 -0500

    try1

---
+++ unsupervised_learning/autoencoders/0-vanilla.py
@@ -0,0 +1,35 @@
+#!/usr/bin/envpython3
+'''task 2'''
+import tensorflow.keras as keras
+
+
+def autoencoder(input_dims, hidden_layers, latent_dims):
+    '''creates an autoencoder'''
+
+    inputs = keras.Input(shape=(input_dims,))
+    x = inputs
+
+    for i in hidden_layers:
+        x = keras.layers.Dense(i, activation='relu')(X)
+        outputs = keras.layers.Dense(latent_dims, activation='relu')(X)
+
+        encoder = keras.Model(inputs, outputs, name='encoder')
+
+        # decode
+        latinput = keras.Input(shape=(latent_dims))
+        x = latinput
+    for i in reversed(hidden_layers):
+        x = keras.layers.Dense(i, activation='relu')(x)
+    decout = keras.layers.Dense(input_dims, activation='sigmoid')(x)
+
+    decoder = keras.Model(latinput, decout, name='decoder')
+
+    # Autoencoder
+    autoencoder_inputs = keras.Input(shape=(input_dims,))
+    encoded = encoder(autoencoder_inputs)
+    decoded = decoder(encoded)
+    autoencoder = keras.Model(autoencoder_inputs, decoded, name='autoencoder')
+
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+    return encoder, decoder, autoencoder
