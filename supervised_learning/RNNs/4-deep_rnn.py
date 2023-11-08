0514e5bad9e97efdfd3f139e24b4816d939aae95
gavintobin <gavintobin714@gmail.com>
2023-11-08 09:32:03 -0600

    try1

--- supervised_learning/RNNs/4-deep_rnn.py
+++ supervised_learning/RNNs/4-deep_rnn.py
@@ -6,8 +6,8 @@ import numpy as np
 
 def deep_rnn(rnn_cells, X, h_0):
     ''' forward prop for deep rnn'''
-    t, m, i = X.shape
-    l = len(rnn_cells)
+    t, m, _ = X.shape
+    lit = len(rnn_cells)
     h = h_0.shape[2]
     H = np.zeros((t + 1, l, m, h))
     Y = np.zeros((t, m, rnn_cells[-1].Wy.shape[1]))
