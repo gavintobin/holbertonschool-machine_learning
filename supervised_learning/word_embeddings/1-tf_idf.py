34466265828238d77a3b3b70f165193cdbd99e18
gavintobin <gavintobin714@gmail.com>
2023-11-28 10:22:31 -0600

    try1

---
+++ supervised_learning/word_embeddings/1-tf_idf.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+'''task 2'''
+
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+import numpy as np
+
+def tf_idf(sentences, vocab=None):
+    '''tf idf'''
+
+    if vocab is None:
+        vocab = []
+
+        # make vocab
+        for sentence in sentences:
+            words = sentence.split()
+            vocab.extend(words)
+
+    #  makeTF-IDF vect
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+
+    #  put them into embeddings
+    embeddings = vectorizer.fit_transform(sentences).toarray()
+
+    # get features
+    features = vectorizer.get_feature_names_out()
+
+    return embeddings, features
