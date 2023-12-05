64eba1dfb8d7d0409625974afefda502fb9c566b
gavintobin <gavintobin714@gmail.com>
2023-12-05 08:56:01 -0600

    try1

---
+++ supervised_learning/object_detection/0-yolo.py
@@ -0,0 +1,15 @@
+#!?usr/bin/env python3
+'''task 1'''
+import numpy as np
+from tensorflow.keras.models import load_model
+
+
+class Yolo:
+    def __init__(self, model_path, classes_path, class_t, nms_t, anchors):
+        '''init yolo class'''
+        self.model = load_model(model_path)
+        with open(classes_path, 'r') as file:
+            self.class_names = [line.strip() for line in file]
+        self.class_t = class_t
+        self.nms_t = nms_t
+        self.anchors = anchors
