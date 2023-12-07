cbe630d29568fba2173b39d3c6483a1aba9aa950
gavintobin <gavintobin714@gmail.com>
2023-12-07 12:36:02 -0600

    try1

--- supervised_learning/object_detection/1-yolo.py
+++ supervised_learning/object_detection/1-yolo.py
@@ -26,29 +26,29 @@ class Yolo:
             box_class_probs.append(self.sigmoid(output[..., 5:]))
 
         all_boxes = []
-        for output_boxes, box_confidence, box_class_prob in zip(boxes, box_confidences, box_class_probs):
-            grid_height, grid_width, anchors = output_boxes.shape[:3]
+        for output in range(len(boxes)):
+            grid_height = outputs[output].shape[0]
+            grid_width = outputs[output].shape[1]
+            anchors = outputs[output].shape[2]
 
             for cy in range(grid_height):
                 for cx in range(grid_width):
                     for b in range(anchors):
-                        confidence = float(box_confidence[cy, cx, b])
-                        class_probs = box_class_prob[cy, cx, b, :]
+                        pw, ph = self.anchors[b][:2]
+                        tx, ty, tw, th = boxes[output][cy, cx, b]
+                        bx = (self.sigmoid(tx) + cx) / grid_width
+                        by = (self.sigmoid(ty) + cy) / grid_height
+                        bw = pw * np.exp(tw) / self.model.input.shape[1].value
+                        bh = ph * np.exp(th) / self.model.input.shape[2].value
 
-                        if confidence > self.class_t:
-                            pw, ph = self.anchors[b][:2]
-                            tx, ty, tw, th = output_boxes[cy, cx, b]
-                            bx = (self.sigmoid(tx) + cx) / grid_width
-                            by = (self.sigmoid(ty) + cy) / grid_height
-                            bw = pw * np.exp(tw) / self.model.input.shape[1].value
-                            bh = ph * np.exp(th) / self.model.input.shape[2].value
+                        x1 = max(int((bx - (bw / 2)).any() * image_width), 0)
+                        x2 = min(int((bx + (bw / 2)).any() * image_width),
+                                 image_width)
+                        y2 = min(int((by + (bh / 2)).any() * image_height),
+                                 image_height)
+                        y1 = max(int((by - (bh / 2)).any() * image_height), 0)
 
-                            x1 = max(int((bx - (bw / 2)).any() * image_width), 0)
-                            y1 = max(int((by - (bh / 2)).any() * image_height), 0)
-                            x2 = min(int((bx + (bw / 2)).any() * image_width), image_width)
-                            y2 = min(int((by + (bh / 2)).any() * image_height), image_height)
-
-                            all_boxes.append([x1, y1, x2, y2])
+                        all_boxes.append([x1, y1, x2, y2])
 
         return np.array(all_boxes), box_confidences, box_class_probs
 
